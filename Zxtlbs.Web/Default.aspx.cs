3cf2debf94ad5aa7a99b3a1570d312a017190d08
Administrator <Administrator@Exsun-Wukai>
2012-03-05 17:02:06 +0800

    2

--- Zxtlbs.Web/Default.aspx.cs
+++ Zxtlbs.Web/Default.aspx.cs
@@ -2,22 +2,20 @@
 using System.Collections;
 using System.Configuration;
 using System.Data;
-using System.Linq;
 using System.Web;
 using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
-using System.Xml.Linq;
 
-namespace zxtlbs
+namespace Zxtlbs.Web
 {
     public partial class _Default : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
